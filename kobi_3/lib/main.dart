4c5a69058c8f6bb8cf5a75a7fde9b148a0c5b17a
mjkim0928 <june573166@naver.com>
2024-04-11 18:37:50 +0900

    mj first commit

--- kobi_3/lib/main.dart
+++ kobi_3/lib/main.dart
@@ -1,32 +1,8 @@
-import 'dart:io';
-
-import 'package:flutter/services.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'login.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import './login.dart';
 
-main() async {
-  // 가로모드 비활성화
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitUp,
-  ]);
-  _initNotiSetting();
-
+void main() {
   runApp(MaterialApp(home: MyApp()));
-}
-
-void _initNotiSetting() async {
-  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  final initSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
-  final initSettings = InitializationSettings(
-    android: initSettingsAndroid,
-  );
-  await flutterLocalNotificationsPlugin.initialize(
-    initSettings,
-  );
 }
 
 class MyApp extends StatefulWidget {
@@ -38,82 +14,13 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    AgreeDialog();
-  }
-
-  void InfoDialog() async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-
-    String appName = packageInfo.appName;
-    String packageName = packageInfo.packageName;
-    String version = packageInfo.version;
-    String buildNumber = packageInfo.buildNumber;
-
-
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          // 둥근 모서리
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-          // Dialog Main Title
-          title: Column(
-            children: [
-              new Text(
-                "앱 및 개발자 정보",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("앱 버전: $version"),
-              Text(" "),
-              Text("[Contact]"),
-              Text("Email: msh050533@gmail.com"),
-            ],
-          ),
-          actions: [
-            TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "확인",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                      color: Colors.purple, // 텍스트 색상을 여기서 설정합니다.
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                     Colors.purple, // 버튼의 전경색을 설정합니다. (예: 텍스트 색상)
-                  ),
-                ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CNU Dorm 자가진단',
-      theme: ThemeData(primarySwatch: Colors.purple, accentColor: Colors.amber),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('KOBI: 코리아텍 비서'),
-          actions: [
-            IconButton(
-                onPressed: () => InfoDialog(),
-                icon: Icon(Icons.info_outline))
+          title: const Text('CNU Dorm 자가진단'),
+          actions: [Icon(Icons.calendar_month)
           ],
         ),
         body: GestureDetector(
