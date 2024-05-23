763013b7ab2dc9f784cdade2e972f83036c23817
mjkim0928 <june573166@naver.com>
2024-05-23 22:42:48 +0900

    port forwarding test

--- kobi_3/lib/login.dart
+++ kobi_3/lib/login.dart
@@ -103,7 +103,7 @@ class _LoginPageState extends State<Login> {
   }
 
   Future<http.Response> _makeLoginRequest() {
-    var url = 'http://211.57.218.130:37627/login';
+    var url = 'http://192.168.219.101:5000/login';
     return http.post(
       Uri.parse(url),
       headers: {'Content-Type': 'application/json'},
