e0d724a5e4a47cd99d40652f673004e8b55898d8
LogiStic1207 <152528155+LogiStic1207@users.noreply.github.com>
2024-05-29 01:24:22 +0900

    final commit

--- kobi_3/lib/main.dart
+++ kobi_3/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'KOBI: 코리아텍 비서',
       home: ConsentChecker(),
     );
